ab21d6c0463994922871ea16dbc93f5308183000
MurilloPsc <murillopereira@outlook.com>
2019-08-14 22:28:50 -0300

    Exercicio 2

---
+++ ex2.js
@@ -0,0 +1,5 @@
+var comprimento = 3;
+var largura = 7;
+var altura = 2;
+var volume = function (comprimento, largura, altura) { return (comprimento * largura * altura); };
+console.log("O Volume \u00E9 : " + volume(comprimento, largura, altura));
---
+++ ex2.ts
@@ -0,0 +1,7 @@
+let comprimento : number = 3
+let largura : number = 7
+let altura : number = 2
+
+let volume = (comprimento:number, largura:number, altura:number)=>(comprimento*largura*altura)
+
+console.log(`O Volume é : ${volume(comprimento,largura,altura)}`)
